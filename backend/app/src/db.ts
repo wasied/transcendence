164afdf52c2a7f910b626f1435775ace02a1cfc7
Spider-man <>
2023-07-16 11:35:31 +0200

    Fix db connection

--- backend/app/src/db.ts
+++ backend/app/src/db.ts
@@ -2,7 +2,7 @@ import { Client } from 'ts-postgres';
 
 const config = {
 	"host": process.env.POSTGRES_DB_HOST,
-	"port": process.env.POSTGRES_DB_PORT,
+	"port": +process.env.POSTGRES_DB_PORT,
 	"user": process.env.POSTGRES_DB_USER,
 	"database": process.env.POSTGRES_DB_NAME,
 	"password": process.env.POSTGRES_DB_PASS,
