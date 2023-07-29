a7e0fbfb4bcb3016d684d0cd702d109934c61692
Corentin JULIENNE <68602274+Corentin-Julienne@users.noreply.github.com>
2023-07-29 13:59:51 -0600

    add auth service

--- frontend/app/src/app/services/auth.service.ts
+++ frontend/app/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
 	providedIn: 'root'
@@ -9,8 +10,23 @@ export class AuthService {
 	constructor (private http: HttpClient) {};
 
 	private apiURL: string = 'http://localhost:8080/auth/login'; // change that
+	authURL!: string;
+	authObs$!: Observable<string>;
+
+	retrieveURL() : Observable<string> {
+		return this.http.get<string>(`${this.apiURL}`);
+	}
+
+	subscribeObservable() : void {
+		this.authObs$.subscribe(value => {
+			console.log(value);
+			this.authURL = value;
+		});
+		window.location.href = this.authURL;
+	}
 
 	triggerAuth() : void {
-		this.http.get<void>(`${this.apiURL}`);
+		this.authObs$ = this.retrieveURL();
+		this.subscribeObservable();
 	}
 }
