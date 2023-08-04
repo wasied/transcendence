baf21419a3a48ab59bfd3b17c08aaf49cd2b2786
Corentin JULIENNE <68602274+Corentin-Julienne@users.noreply.github.com>
2023-08-04 15:56:39 -0600

    add comm service for game data

--- frontend/app/src/app/views/friends-view/friends-view.module.ts
+++ frontend/app/src/app/views/friends-view/friends-view.module.ts
@@ -2,17 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 
-import { FriendsViewComponent } from './friends-view/friends-view.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FriendsViewComponent } from './components/friends-view/friends-view.component';
 import { FriendsViewRoutingModule } from './friends-view-routing.module';
+import { DropdownUsersComponent } from './components/dropdown-users/dropdown-users.component';
 
 @NgModule({
-  declarations: [
-    FriendsViewComponent
-  ],
-  imports: [
-    CommonModule,
-    SharedModule,
-    FriendsViewRoutingModule
-  ]
+	declarations: [
+		FriendsViewComponent,
+		DropdownUsersComponent
+	],
+	imports: [
+		CommonModule,
+		SharedModule,
+		FriendsViewRoutingModule,
+		ReactiveFormsModule,
+		MatSelectModule,
+		BrowserAnimationsModule
+	]
 })
 export class FriendsViewModule { }
