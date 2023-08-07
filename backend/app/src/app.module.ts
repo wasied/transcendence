b55eb469e00776a97862834b7843e552f4228a04
mpeharpr <contact@maaxit.fr>
2023-08-08 01:58:33 +0200

    Fully delete all related to websockets since it's FUCKING BROKEN

--- backend/app/src/app.module.ts
+++ backend/app/src/app.module.ts
@@ -6,10 +6,9 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { ChatModule } from './chat/chat.module';
 import { SessionsModule } from './sessions/sessions.module';
-import { WebsocketsModule } from './websockets/websockets.module';
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot(), UsersModule, ChatModule, SessionsModule, WebsocketsModule],
+  imports: [AuthModule, ConfigModule.forRoot(), UsersModule, ChatModule, SessionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
