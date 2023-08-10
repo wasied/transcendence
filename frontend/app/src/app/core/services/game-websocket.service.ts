cbbf878dc39e599479f17662489b8c63df4654b8
mpeharpr <contact@maaxit.fr>
2023-08-10 02:05:24 +0200

    Add explanations in the code

--- frontend/app/src/app/core/services/game-websocket.service.ts
+++ frontend/app/src/app/core/services/game-websocket.service.ts
@@ -25,28 +25,60 @@ export class GameWebsocketService {
 
     private listenToServerEvents(): void {
         this.socket.on('gameStarted', (data: any) => {
-          this.gameStarted$.next(data);
+        	this.gameStarted$.next(data);
         });
         
+		// Get update about the game (data is a structure with all informations needed)
+		// WARNING: EVERYTHING IS IN % (0-1) !!! So x=0.5 means the ball is in the middle of the screen
         this.socket.on('gameUpdate', (data: any) => {
-          this.gameUpdate$.next(data);
+
+			// data.paddleLeft.y
+			// data.paddleRight.y
+			// data.ball.x
+			// data.ball.y
+
+        	this.gameUpdate$.next(data);
+			
         });
     
         this.socket.on('gameEnded', (data: any) => {
-          this.gameEnded$.next(data);
+        	this.gameEnded$.next(data);
         });
     }
 
-    public joinGame(): void {
-        this.socket.emit('joinGame');
+	// This will make the player join the matchmaking queue (the public one) until he finds someone
+	// When he'll find someone, gameStarted will be called
+	public joinMatchmaking(): void {
+        this.socket.emit('joinMatchmaking');
     }
 
+	// This will make the player join the session without actually being a player
+	// So he'll just receive game updates for the match
+    public spectateGame(spectatingUserId: string): void {
+        this.socket.emit('spectateGame', spectatingUserId);
+    }
+
+	// This will update the paddle position of the player serverside and sync it to everyone
     public updatePaddlePosition(state: any): void {
         this.socket.emit('paddlePositionUpdate', state);
     }
 
+	// This will disconnect and stop the party for everyone
+	// gameEnded will be called
     public disconnect(): void {
         this.socket.disconnect();
     }
+
+	// This will make the player invite someone to play with him
+	// It has nothing to do with the chat system but it's needed for the invitation to work
+	public invitePlayer(opponentId: string): void {
+        this.socket.emit('invitePlayer', opponentId);
+    }
+
+	// This will make the player accept the invitation of someone
+	// It has nothing to do with the chat system but it's needed for the invitation to work
+	public acceptInvitation(opponentId: string): void {
+		this.socket.emit('acceptInvitation', opponentId);
+	}
 
 }
