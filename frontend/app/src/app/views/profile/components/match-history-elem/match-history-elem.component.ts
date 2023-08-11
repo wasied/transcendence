70a364fcdd57880aa6b208a4ac2307b513a8c9c0
Corentin JULIENNE <68602274+Corentin-Julienne@users.noreply.github.com>
2023-08-11 17:44:01 -0600

    fvdrg

--- frontend/app/src/app/views/profile/components/match-history-elem/match-history-elem.component.ts
+++ frontend/app/src/app/views/profile/components/match-history-elem/match-history-elem.component.ts
@@ -1,17 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { MatchHistory } from 'src/app/core/models/match-history.model';
 import { Router } from '@angular/router';
+import { UsersService } from '../../../../core/services/users.service';
+import { Observable } from 'rxjs';
 
 @Component({
 	selector: 'app-match-history-elem',
 	templateUrl: './match-history-elem.component.html',
 	styleUrls: ['./match-history-elem.component.css']
 })
-export class MatchHistoryElemComponent {
+export class MatchHistoryElemComponent implements OnChanges {
 
 	@Input() matchHistoryElem!: MatchHistory;
+	matchHistory$!: Observable<MatchHistory>;
 
-	constructor (private router: Router) {};
+	constructor (private router: Router, private usersService: UsersService) {};
+
+	ngOnChanges(changes: SimpleChanges): void {
+		if (changes['userId'] && changes['userId'].currentValue) {
+			this.user$ = this.usersService.getUserById(changes['userId'].currentValue);
+		}
+	}
 
 	goToOpponentProfile(opponentId: number) : void {
 		this.router.navigate(['main', 'profile', opponentId]);
