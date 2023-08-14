117e7371d176b0e79ac900ccbaabd889e175995e
Maxime <72105771+MaaxIT@users.noreply.github.com>
2023-08-14 03:58:54 +0200

    Update direct-messages-view.component.ts

--- frontend/app/src/app/views/direct-messages/components/direct-messages-view/direct-messages-view.component.ts
+++ frontend/app/src/app/views/direct-messages/components/direct-messages-view/direct-messages-view.component.ts
@@ -53,9 +53,9 @@ export class DirectMessagesViewComponent implements OnInit, OnDestroy {
 		this.isModalOpen = false;
 	}
 
-	ngOnDestroy(): void {
-		if (this.subscription) {
-			this.subscription.unsubscribe();
-		}
-	}
+	// ngOnDestroy(): void {
+	// 	if (this.subscription) {
+	// 		this.subscription.unsubscribe();
+	// 	}
+	// }
 }
